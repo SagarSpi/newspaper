be1489fda2945e86276729458bd4c4410160c73f
Sagar Mondal <sagarspi583@gmail.com>
2025-02-03 22:33:29 +0600

    add assor article modal

--- app/Models/Backend/Article.php
+++ app/Models/Backend/Article.php
@@ -3,6 +3,7 @@
 namespace App\Models\Backend;
 
 use App\Models\Backend\User;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -13,4 +14,15 @@ class Article extends Model
     public function user() {
         return $this->belongsTo(User::class)->select(['id', 'name', 'email', 'contacts', 'role']);
     }
+    protected function CreatedAt() : Attribute{
+        return Attribute::make(
+            get: fn(string $value)=> date('d M Y', strtotime($value))
+        );
+    }
+    protected function UpdatedAt(): Attribute {
+        return Attribute::make(
+            get: fn(string $value)=>date('d M Y',strtotime($value))
+        );
+    }
+
 }
